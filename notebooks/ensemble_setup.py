bc58ad7d483d9a1d3296661b4dc1e336b2cb8c85
tobfer <soutobias@yahoo.com.br>
2024-03-01 16:10:19 +0000

    add jennifer notebooks

---
+++ notebooks/ensemble_setup.py
@@ -0,0 +1,7 @@
+
+# Directory on JASMIN that contains the CANARI-LE data:
+datadir = '/gws/nopw/j04/canari/shared/large-ensemble/priority/HIST2/'
+
+# List of emsemble members with completed priority variables:
+ens = [1,  2,  3,  4, 5,  6, 7, 9, 10,  12,  13,  18, 2,  20,  21,  22,  24,  28, 31, ]
+
